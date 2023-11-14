25a4afaa9524a70746f913080ec93f4bd3411709
acs0209 <annjae4676@naver.com>
2023-11-14 15:28:30 +0900

    refactor: 개행 문자 수정

--- src/main/java/christmas/view/constant/NoEventOutputConstant.java
+++ src/main/java/christmas/view/constant/NoEventOutputConstant.java
@@ -1,11 +1,11 @@
 package christmas.view.constant;
 
 public enum NoEventOutputConstant {
-    GIFT_MENU("<증정 메뉴>\r\n없음"),
-    BENEFIT_DETAILS("<혜택 내역>\r\n없음"),
-    TOTAL_BENEFIT_AMOUNT("<총혜택 금액>\r\n0원"),
-    EXPECTED_PAYMENT_AFTER_DISCOUNT("<할인 후 예상 결제 금액>\r\n%s원"),
-    DECEMBER_EVENT_BADGE("<12월 이벤트 배지>\r\n없음");
+    GIFT_MENU("<증정 메뉴>\n없음"),
+    BENEFIT_DETAILS("<혜택 내역>\n없음"),
+    TOTAL_BENEFIT_AMOUNT("<총혜택 금액>\n0원"),
+    EXPECTED_PAYMENT_AFTER_DISCOUNT("<할인 후 예상 결제 금액>\n%s원"),
+    DECEMBER_EVENT_BADGE("<12월 이벤트 배지>\n없음");
 
     private final String message;
 
