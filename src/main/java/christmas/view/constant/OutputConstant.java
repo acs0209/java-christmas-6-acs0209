e83734086f709796e8a6ab7c0c945599333b589c
acs0209 <annjae4676@naver.com>
2023-11-14 15:35:26 +0900

    refactor: 개행 문자 수정

--- src/main/java/christmas/view/constant/OutputConstant.java
+++ src/main/java/christmas/view/constant/OutputConstant.java
@@ -4,11 +4,11 @@ public enum OutputConstant {
     PREVIEW_EVENT_BENEFITS("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDER_START("<주문 메뉴>"),
     ORDER_MENU("%s %d개"),
-    PRE_DISCOUNT_ORDER("<할인 전 총주문 금액>\n%s"),
+    PRE_DISCOUNT_ORDER("<할인 전 총주문 금액>\r\n%s"),
     COLON(": "),
     DASH("-"),
     WON("원"),
-    NEW_LINE("\n");
+    NEW_LINE("\r\n");
 
     private final String message;
 
